bfcf82801815e70e0a5c60da3d7fd9a018cfa7bc
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-07-26 17:29:28 +0200

    Add files via upload

---
+++ src/test/java/de/mathisneunzig/md/MarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mathisneunzig.md;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
